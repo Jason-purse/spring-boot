ac16f02b656878599bfdeea94d7dcfad65f33e65
FLJ <617229004@qq.com>
2022-06-06 20:36:45 +0800

    spring boot tools study(设置自己的代理配置)

--- spring-boot-other-tests/src/main/java/club/smileboy/app/MyCustomPlugin.java
+++ spring-boot-other-tests/src/main/java/club/smileboy/app/MyCustomPlugin.java
@@ -1,4 +1,19 @@
 package club.smileboy.app;
 
-public class MyCustomPlugin {
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+   * @author FLJ
+   * @date 2022/6/6
+   * @time 12:50
+   * @Description 我的自定义测试插件 ...(可以是一个脚本,因为build.gradle 相关的.gradle 脚本没有代码提示,通过应用插件的方式,配置project)
+   */
+public class MyCustomPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project target) {
+		target.apply();
+	}
 }
+
